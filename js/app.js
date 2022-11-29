ca6f263cc3e4ad67099538682f97518e132c8613
Jordan Covington <Atlantafan1087@gmail.com>
2022-11-29 15:05:50 -0800

    Cleaned code for JavaScript File

--- js/app.js
+++ js/app.js
@@ -9,69 +9,56 @@ let questionOneGuess = prompt('Am I from North Carolina?').toLowerCase()
 
 if(questionOneGuess === 'no' || questionOneGuess === 'n'){
   alert('You got it right!');
+  score++;
   // console.log('You got it right!');
 } else if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
   alert('Sorry, you got it wrong');
   // console.log('Sorry, you got it wrong');
 }
 
-if(questionOneGuess === 'no' || questionOneGuess === 'n'){
-  score++;
-}
-
 let questionTwoGuess = prompt('Am I married?').toLowerCase()
 
 if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
   alert('You got it right!');
+  score++;
   // console.log('You got it right!');
 } else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
   alert('Sorry, you got it wrong');
   // console.log('Sorry, you got it wrong');
 }
 
-if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
-  score++;
-}
-
 let questionThreeGuess = prompt('Have I lived in Japan?').toLowerCase()
 
 if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
   alert('You got it right!');
+  score++;
   // console.log('You got it right!');
 } else if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
   alert('Sorry, you got it wrong');
   // console.log('Sorry, you got it wrong');
 }
 
-if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
-  score++;
-}
-
 let questionFourGuess = prompt('Was I ever in the Army?').toLowerCase()
 
 if(questionFourGuess === 'no' || questionFourGuess === 'n'){
   alert('You got it right!');
+  score++;
   // console.log('You got it right!');
 } else if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
   alert('Sorry, you got it wrong');
   // console.log('Sorry, you got it wrong');
 }
 
-if(questionFourGuess === 'no' || questionFourGuess === 'n'){
-  score++;
-}
 let questionFiveGuess = prompt('Do I have two daughters?').toLowerCase()
 
 if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
   alert('You got it right!');
+  score++;
   // console.log('You got it right!');
 } else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
   alert('Sorry, you got it wrong');
   // console.log('Sorry, you got it wrong');
 }
 
-if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
-  score++
-}
 
 alert(`All right ${userName} you got ${score} out of 5 correct!`);
