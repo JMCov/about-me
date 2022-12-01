a6d34249e35393927d6d899029494c3da8b25c31
Brandon Perard <brandon.perard@gmail>
2022-12-01 13:56:11 -0800

    Refactor JCov About Me

--- js/app.js
+++ js/app.js
@@ -5,94 +5,116 @@ let randomNum = 12;
 let i = 3;
 let countriesArray = ['bolivia', 'canada', 'germany', 'australia', 'egypt', 'vietnam'];
 
-let userName = prompt('What is your name?');
-alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
+function questionOne() {
+  let userName = prompt('What is your name?');
+  alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
 
-let questionOneGuess = prompt('Am I from North Carolina?').toLowerCase();
+  let questionOneGuess = prompt('Am I from North Carolina?').toLowerCase();
 
-if(questionOneGuess === 'no' || questionOneGuess === 'n'){
-  alert('You got it right!');
-  score++;
-  // console.log('You got it right!');
-} else if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
-  alert('Sorry, you got it wrong');
-  // console.log('Sorry, you got it wrong');
-}
-
-let questionTwoGuess = prompt('Am I married?').toLowerCase();
-
-if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
-  alert('You got it right!');
-  score++;
-  // console.log('You got it right!');
-} else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
-  alert('Sorry, you got it wrong');
-  // console.log('Sorry, you got it wrong');
-}
-
-let questionThreeGuess = prompt('Have I lived in Japan?').toLowerCase();
-
-if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
-  alert('You got it right!');
-  score++;
-  // console.log('You got it right!');
-} else if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
-  alert('Sorry, you got it wrong');
-  // console.log('Sorry, you got it wrong');
-}
-
-let questionFourGuess = prompt('Was I ever in the Army?').toLowerCase();
-
-if(questionFourGuess === 'no' || questionFourGuess === 'n'){
-  alert('You got it right!');
-  score++;
-  // console.log('You got it right!');
-} else if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
-  alert('Sorry, you got it wrong');
-  // console.log('Sorry, you got it wrong');
-}
-
-let questionFiveGuess = prompt('Do I have two daughters?').toLowerCase();
-
-if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
-  alert('You got it right!');
-  score++;
-  // console.log('You got it right!');
-} else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
-  alert('Sorry, you got it wrong');
-  // console.log('Sorry, you got it wrong');
-}
-
-let questionSixGuess = prompt('Guess a number from 1-100!');
-
-while (questionSixGuess !== randomNum && i > 0){
-  if (questionSixGuess > randomNum){
-    questionSixGuess = prompt(`Wrong my number is lower than ${questionSixGuess}! Guess again!`);
-    i--;
-  }else if (questionSixGuess < randomNum){
-    questionSixGuess = prompt(`Wrong my number is higher than ${questionSixGuess}! Guess again!`);
-    i--;
-  }else{
-    alert(`Well Done ${userName}, ${randomNum} is the answer!`);
+  if (questionOneGuess === 'no' || questionOneGuess === 'n') {
+    alert('You got it right!');
     score++;
-    break;
-  }
-  if (i === 0){
-    alert(`The correct answer is ${randomNum}`);
+    // console.log('You got it right!');
+  } else if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
+    alert('Sorry, you got it wrong');
+    // console.log('Sorry, you got it wrong');
   }
 }
 
-for (i = 0; i < 6; i++){
-  let questionSevenGuess = prompt('Name one of the countries I am thinking about!').toLowerCase();
-  if (countriesArray.indexOf(questionSevenGuess) >= 0){
-    alert(`Good job ${userName}! ${questionSevenGuess} is correct`);
+function questionTwo() {
+  let questionTwoGuess = prompt('Am I married?').toLowerCase();
+
+  if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
+    alert('You got it right!');
     score++;
-    break;
-  } else {
-    alert('That is not one of the countries I am thinking about. Try Again!');
+    // console.log('You got it right!');
+  } else if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
+    alert('Sorry, you got it wrong');
+    // console.log('Sorry, you got it wrong');
   }
 }
 
-alert(`All possible answers were: ${countriesArray}`);
+function questionThree() {
+  let questionThreeGuess = prompt('Have I lived in Japan?').toLowerCase();
+
+  if (questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
+    alert('You got it right!');
+    score++;
+    // console.log('You got it right!');
+  } else if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
+    alert('Sorry, you got it wrong');
+    // console.log('Sorry, you got it wrong');
+  }
+}
+
+function questionFour() {
+  let questionFourGuess = prompt('Was I ever in the Army?').toLowerCase();
+
+  if (questionFourGuess === 'no' || questionFourGuess === 'n') {
+    alert('You got it right!');
+    score++;
+    // console.log('You got it right!');
+  } else if (questionFourGuess === 'yes' || questionFourGuess === 'y') {
+    alert('Sorry, you got it wrong');
+    // console.log('Sorry, you got it wrong');
+  }
+}
+
+function questionFive() {
+  let questionFiveGuess = prompt('Do I have two daughters?').toLowerCase();
+
+  if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
+    alert('You got it right!');
+    score++;
+    // console.log('You got it right!');
+  } else if (questionFiveGuess === 'no' || questionFiveGuess === 'n') {
+    alert('Sorry, you got it wrong');
+    // console.log('Sorry, you got it wrong');
+  }
+}
+
+function questionSix() {
+  let questionSixGuess = prompt('Guess a number from 1-100!');
+
+  while (questionSixGuess !== randomNum && i > 0) {
+    if (questionSixGuess > randomNum) {
+      questionSixGuess = prompt(`Wrong my number is lower than ${questionSixGuess}! Guess again!`);
+      i--;
+    } else if (questionSixGuess < randomNum) {
+      questionSixGuess = prompt(`Wrong my number is higher than ${questionSixGuess}! Guess again!`);
+      i--;
+    } else {
+      alert(`Well Done ${userName}, ${randomNum} is the answer!`);
+      score++;
+      break;
+    }
+    if (i === 0) {
+      alert(`The correct answer is ${randomNum}`);
+    }
+  }
+}
+
+function questionSeven() {
+  for (i = 0; i < 6; i++) {
+    let questionSevenGuess = prompt('Name one of the countries I am thinking about!').toLowerCase();
+    if (countriesArray.indexOf(questionSevenGuess) >= 0) {
+      alert(`Good job ${userName}! ${questionSevenGuess} is correct`);
+      score++;
+      break;
+    } else {
+      alert('That is not one of the countries I am thinking about. Try Again!');
+    }
+  }
+
+  alert(`All possible answers were: ${countriesArray}`);
+}
+
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
+questionSix();
+questionSeven();
 
 alert(`All right ${userName} you got ${score} out of 7 correct!`);
